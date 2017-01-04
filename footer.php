17f717cdf4e51304b114c9e8c80480ddc4c77623
Filcis <cislakfilip@gmail.com>
2017-01-04 14:53:20 +0100

    footer basic styling

--- footer.php
+++ footer.php
@@ -15,9 +15,6 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'rk90' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'rk90' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'rk90' ), 'rk90', '<a href="https://automattic.com/" rel="designer">Filip Cislak</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
