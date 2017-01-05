772a6d9f362a68b133149fdaf77bcccfc32b0ab7
Filcis <cislakfilip@gmail.com>
2017-01-05 16:02:47 +0100

    push shortcode

--- inc/rk90-shortcodes.php
+++ inc/rk90-shortcodes.php
@@ -10,8 +10,9 @@ add_shortcode( 'rk90_row', 'rk90_row' );
 function rk90_column( $atts, $content ) {
     $atts = shortcode_atts( array(
         'small' => 12,
-        'medium' => null,
-        'large' => null
+        'medium'=> null,
+        'large' => null,
+        'push'  => null
     ), $atts );
     
     $atts['medium'] = ( $atts['medium'] == null ) ? $atts['small'] : $atts['medium'];
@@ -20,8 +21,12 @@ function rk90_column( $atts, $content ) {
     extract( $atts );
     
     $sizes = 'small-' . $small . ' medium-' . $medium . ' large-' . $large;
+    $push = '';
+    if ( !empty($atts['push']) ) {
+        $push = ' push-' . $atts['push'];
+    } 
     
-    return '<div class="columns ' . $sizes . '">' . do_shortcode( $content ) . '</div>';
+    return '<div class="columns ' . $sizes . $push . '">' . do_shortcode( $content ) . '</div>';
 }
 
 add_shortcode( 'rk90_column', 'rk90_column');
